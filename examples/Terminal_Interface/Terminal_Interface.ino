214fa325b8f5f87ba5f8a25d456dc0f1c47edf61
Max 'MaxMax' Mönikes <m.moenikes@fasttube.de>
2024-06-22 01:01:50 +0200

    Added Helppage to Terminal Example

--- examples/Terminal_Interface/Terminal_Interface.ino
+++ examples/Terminal_Interface/Terminal_Interface.ino
@@ -1,3 +1,28 @@
+/*
+MIT License
+
+Copyright (c) 2024 Maximilian Mönikes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
 #include "TLE9012.h"
 #include <stdio.h>
 
@@ -15,6 +40,18 @@ uint32_t last_wd_trigger = 0;
 uint8_t terminalbufferrecvlen = 0;
 char terminalbuffer[64];
 
+//constants
+const char helppage[] = 
+
+"Arduino based UART-isoUART Bridge v1\n \
+The following commands are implemented:\n \
+\n\
+ -IL: no function, only exists for compatibility reasons\n\
+ -IH: no function, only exists for compatibility reasons\n\
+ -WH/WL device_address register_address data : Write data to a register\n\
+ -RH/RL device_address register_address : Read data from a register\n\
+ -K update_rate : Reset Watchdog every update_rate interval in milliseconds\n\
+ -? Show Manual\n";
 
 void setup() { 
   Serial.begin(115200); //Start Console interface
@@ -187,6 +224,12 @@ void interpretCommand(const char commandbuffer[], uint8_t bufferlength)
           }
         }
       }
+      
+      else if(!strcmp(cmd,"?")) //Print Helppage
+      {
+        Serial.print(helppage);
+      }
+      
       else //No valid command was detected
       {
         Serial.println("INVALID COMMAND");
